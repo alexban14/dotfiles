ac4cd78f6ee8759f9d5824e8fe8f42f9596711e9
Alex Ban <alexbanut10@gmail.com>
2026-06-04 19:30:07 +0300

    dotfiles: zshrc_cofig robustness ($HOME + guard ng), kitty font, nvim packer colorscheme pcall
    - bash/zshrc_cofig: PATH uses $HOME (was hardcoded /home/banalexandru); guard
      `ng completion` so a missing Angular CLI doesn't error every shell start
    - terminal_env/kitty/kitty.conf: font_family CaskaydiaCove NFM (was an unset font)
    - nvim packer.lua: pcall the rose-pine colorscheme so a missing plugin doesn't error

--- development_env/nvim/lua/banalexandru/packer.lua
+++ development_env/nvim/lua/banalexandru/packer.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function(use)
   }
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
-  vim.cmd('colorscheme rose-pine')
+  pcall(vim.cmd, 'colorscheme rose-pine')
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
